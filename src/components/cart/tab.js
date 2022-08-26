96afa7470e2ff702a4bdff7f375bebcae8a306bc
uyriq <uyriq@local>
2022-08-26 22:25:08 +0300

    bug: ошибка в объявлении dispatch 3/3

--- src/components/cart/tab.js
+++ src/components/cart/tab.js
@@ -1,22 +1,17 @@
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { TAB_SWITCH } from '../../services/actions/cart'
 import styles from './tab.module.css'
 
 export const Tab = ({ text, active, onClick: handleClick, value }) => {
     const dispatcher = useDispatch()
-
-    const onClick = React.useCallback(() => {
-        if (typeof handleClick === 'function') {
-            handleClick(value)
-        }
-    }, [handleClick, value])
 
     const switchTab = () => {
         dispatcher({ type: TAB_SWITCH })
     }
     const className = `${styles.tab} ${active ? styles.tab_type_current : ''}`
     return (
-        <div className={`${className}`} onClick={onClick}>
+        <div className={`${className}`} onClick={switchTab}>
             {text}
         </div>
     )
